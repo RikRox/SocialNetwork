94ea5f5a136fdecee1006996d48228fe96cb7ac8
RikRox <104681617+RikRox@users.noreply.github.com>
2022-10-07 12:30:11 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+mongoose.connect(
+    process.env.MONDGODB_URI || 'mongodb://127.9.9.1:27017/SociialNetwork',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
+
+//use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.use(require('./routes'));
+
+app.listen(PORT, () => console.log(`connected on localhost: ${PORT}`));
